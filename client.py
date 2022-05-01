796d3d8e1b2fedf7c4171f4ef45b8ce2b2cd8515
mapl <placenti.marco@gmail.com>
2022-05-01 17:38:03 +0200

    working client to consume model

--- client.py
+++ client.py
@@ -28,7 +28,7 @@ def mode_2():
     print("Option 2")
     pipe = dp()
     pipe.init(name='ImageClassificationV2', task='classification')
-    pipe.preproc_data(location='data/source/MNISTMini/', img_res=[28,28], greyscale=False, test_size=0.2, folds=2)
+    pipe.preproc_data(location='data/MNISTMini/', img_res=[28,28], greyscale=False, test_size=0.2, folds=2)
     pipe.train(max_epochs=1, batch_size=[64, 128], optimizer='Adam', learning_rate=[0.0001, 0.01], number_trials=3)
     pipe.eval()
     pipe.deploy()
